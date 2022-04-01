6b1ebc36b611d80060f58b9b28166946e1bce1d2
Nischal Pokharel <pokharelnischal567@gmail.com>
2022-04-01 20:41:51 +0545

    automation for login page of puryau with multiple data

--- DataDrivenTestCases.py
+++ DataDrivenTestCases.py
@@ -1,6 +1,8 @@
 import xlutils
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 
 driver = webdriver.Chrome(executable_path="C:\selenium drivers\chromedriver_win32\chromedriver")
 # driver.get('https://demo.guru99.com/test/newtours/')
@@ -18,22 +20,29 @@ for r in range(2,rows+1):
 
     # driver.find_element(By.NAME,'userName').send_keys(username)
     # driver.find_element(By.NAME,'password').send_keys(password)
-    email = driver.find_element_by_id("email").send_keys(username)
+    driver.find_element_by_id("email").send_keys(username)
     driver.find_element_by_id("password").send_keys(password)
 
     # driver.find_element(By.NAME,'submit').click()
     driver.find_element_by_xpath("/html/body/div[3]/div/div/div[2]/div/div/div[2]/form/div[3]/div/button").click()
 
 
+
     # if driver.title == "Login: Mercury Tours":
     if driver.title == "Puryau - On Demand Delivery । All Nepal Home Delivery Cargo & Courier":
         print("Test Passed")
         xlutils.writeData(path,'Sheet1',r,3,'Pass')
+        driver.find_element_by_xpath('//*[@id="topnav"]/div[1]/div/div[1]/ul[2]/li[4]/a/img').click()
+        logout = driver.find_element_by_xpath('//*[@id="topnav"]/div[1]/div/div[1]/ul[2]/li[4]/div/a[3]')
+        action = ActionChains(driver)
+        action.move_to_element(logout).click().perform()
+        driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div/div/div/div/div/a[1]').click() 
+
     else:
         print("Test Failed")
         xlutils.writeData(path,'Sheet1',r,3,'Fail')
 
-    driver.find_element_by_xpath('//*[@id="topnav"]/div[1]/div/div[1]/ul[2]/li[4]/a/img').click()
-    driver.find_element_by_xpath('//*[@id="topnav"]/div[1]/div/div[1]/ul[2]/li[4]/div/a[3]').click()
-
+    
+time.sleep(3)
+driver.close()
     # driver.find_element_by_link_text('Home').click()
